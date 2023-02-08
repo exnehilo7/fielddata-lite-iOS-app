5e651967af7fc3aa09b76af2322b7f8efc1080b5
Dan Hopp <exnehilo7@gmail.com>
2023-02-07 21:38:56 -0500

    Prepped for map insertion?

--- FERN/Classes.swift
+++ FERN/Classes.swift
@@ -50,23 +50,23 @@ extension LocationHelper: CLLocationManagerDelegate {
 }
 
 
-// JSON response decoding help from https://www.hackingwithswift.com/quick-start/concurrency/how-to-download-json-from-the-internet-and-decode-it-into-any-codable-type
-extension URLSession {
-    func decode<T: Decodable>(
-        _ type: T.Type = T.self,
-        from url: URL,
-        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
-        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
-        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
-    ) async throws  -> T {
-        let (data, _) = try await data(from: url)
-
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = keyDecodingStrategy
-        decoder.dataDecodingStrategy = dataDecodingStrategy
-        decoder.dateDecodingStrategy = dateDecodingStrategy
-
-        let decoded = try decoder.decode(T.self, from: data)
-        return decoded
-    }
-}
+//// JSON response decoding help from https://www.hackingwithswift.com/quick-start/concurrency/how-to-download-json-from-the-internet-and-decode-it-into-any-codable-type
+//extension URLSession {
+//    func decode<T: Decodable>(
+//        _ type: T.Type = T.self,
+//        from url: URL,
+//        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+//        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
+//        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
+//    ) async throws  -> T {
+//        let (data, _) = try await data(from: url)
+//
+//        let decoder = JSONDecoder()
+//        decoder.keyDecodingStrategy = keyDecodingStrategy
+//        decoder.dataDecodingStrategy = dataDecodingStrategy
+//        decoder.dateDecodingStrategy = dateDecodingStrategy
+//
+//        let decoded = try decoder.decode(T.self, from: data)
+//        return decoded
+//    }
+//}
