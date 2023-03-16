f85a7cfb257fc6141c8bb52ffdee0fd6a37cb37c
Dan Hopp <exnehilo7@gmail.com>
2023-03-16 12:57:14 -0400

    EoS integration is on hold

--- FERN/FERN-Bridging-Header.h
+++ FERN/FERN-Bridging-Header.h
@@ -1,24 +1,21 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
+// The .h has no first-variable definition skips in ViewController.m, but this .h does not recognize @objc
 
 //import Foundation
 //import SwiftUI
 
 //@objc class GPSFeedViewFactory: NSObject {
 //    
-//    @objc static func create(text: String) -> UIViewController {
-//        let gpsFeedView = GPSFeedView(text: text)
+//    @objc static func bridgeGPSFeedUI(latitude: String, longitude: String, altitude: String,
+//                             xyAccuracy: String, gpsUsed: String) -> UIViewController {
+//        let gpsFeedView = GPSFeedView(latitude: latitude, longitude: longitude,
+//                                      altitude: altitude, xyAccuracy: xyAccuracy,
+//                                      gpsUsed: gpsUsed)
 //        let hostingController = UIHostingController(rootView: gpsFeedView)
 //        
 //        return hostingController
 //    }
 //}
 
-/*
- @State var latitude: String
- @State var longitude: String
- @State var altitude: String
- @State var xyAccuracy: String
- @State var gpsUsed: String
- */
