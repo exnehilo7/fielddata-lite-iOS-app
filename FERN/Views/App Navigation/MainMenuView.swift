d1fa16a8fadd487c5aee2e88dbc7601fcf89d9f0
Dan Hopp <exnehilo7@gmail.com>
2024-08-08 18:03:58 -0400

    Adjust upload history file. Add file check on Main Menu.

--- FERN/Views/App Navigation/MainMenuView.swift
+++ FERN/Views/App Navigation/MainMenuView.swift
@@ -19,6 +19,7 @@ struct MainMenuView: View {
     @State private var upload = FileUploadClass()
 //    @State private var networkMonitor = NetworkMonitorClass()
 //    @State private var upload = FileUploadActor()
+    @ObservedObject var network = NetworkMonitorClass()
     
     init() {
         let menuListCoordinator = MenuListBridgingCoordinator()
@@ -27,6 +28,13 @@ struct MainMenuView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query var settings: [Settings]
+    @Query var sdTrips: [SDTrip]
+    
+    @State var showUnpluggedBatteryAlert = false
+    @State var showNoNetworkAlert = false
+    @State var showExpensiveNetworkAlert = false
+    @State var showConstrainedNetworkAlert = false
+    @State var networkIsGoodToGo = false
     
     var body: some View {
         
@@ -122,6 +130,7 @@ struct MainMenuView: View {
                 
             }
         
+        // Upload feedback
         VStack {
             if upload.isLoading {
                 Text("Upload is currently in progress for \(upload.currentTripUploading).").font(.system(size: 15))
@@ -141,19 +150,118 @@ struct MainMenuView: View {
 //        }
         
         Text("Version: \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Cannot get version #")").font(.footnote)
-            // Start GPS feed if not already running
             .onAppear(perform: {
+                // Upload pics and any missed files.
+                if !upload.isLoading {
+                    checkForUploads()
+                }
+                // Start GPS feed if not already running
                 startGPS()
                 // Reset previously snapped pic if view was swiped down before image was saved
                 camera.clearCustomData()
                 camera.resetCamera()
                 
-                // Check for uploads
-                upload.getUploadHistories()
+                
             })
+//            .alert("Charge Cable Not Connected", isPresented: $showUnpluggedBatteryAlert) {
+//                Button("OK", action: {checkForExpensiveNetwork()})
+//                Button("Cancel", role: .cancel){showUnpluggedBatteryAlert = false}
+//            } message: {HStack {Text("Continue with upload?")}
+//            }.alert("Device Not Connected to Wi-Fi", isPresented: $showExpensiveNetworkAlert) {
+//                Button("OK", action: {
+//                    if network.isConstrained {
+//                        showExpensiveNetworkAlert = false
+//                        showConstrainedNetworkAlert = true
+//                    } else {
+//                        showExpensiveNetworkAlert = false
+//                        loopThroughTripsAndUpload()
+//                    }
+//                })
+//                Button("Cancel", role: .cancel){showExpensiveNetworkAlert = false}
+//            } message: {HStack {Text("Continue with upload?")}
+//            }.alert("Low Data Mode is Active", isPresented: $showConstrainedNetworkAlert) {
+//                Button("OK", action: {showConstrainedNetworkAlert = false})
+//            } message: { HStack {Text("Low Data Mode can be disabled in iOS settings.")}
+//            }.alert("Network is Not Connected to the Device", isPresented: $showNoNetworkAlert) {
+//                Button("OK", action: {showNoNetworkAlert = false})
+//            } message: { HStack {Text("Is the device connected to Wi-Fi, Cellular, or Ethernet?")}
+//            }
     }
     
     private func startGPS() {
         gps.startGPSFeed(settings: settings)
+    }
+    
+   private func checkForUploads() {
+        Task.detached {
+            await upload.resetVars()
+            // Get upload history
+            await upload.getUploadHistories()
+            // Make list of trip files
+            for trip in await sdTrips {
+                await upload.getLocalFilePaths(tripName: trip.name, folderName: "metadata")
+                await upload.getLocalFilePaths(tripName: trip.name, folderName: "scores")
+                await upload.getLocalFilePaths(tripName: trip.name, folderName: "images")
+            }
+            // See if a file doesn't exist in Upload History
+            if await anyFilesToUpload() {
+                  print("There are files to upload!")
+                  // Check network connections
+//                     await isNetworkGood()
+              } else {print("No new files to upload.")}
+        }
+
+
+    }
+    
+    private func anyFilesToUpload() async -> Bool {
+        for tripfile in upload.fileList {
+            if !upload.uploadHistoryFileList.contains(tripfile) {
+                print (tripfile)
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func isNetworkGood() {
+        // Check network connections
+        if network.isActive {
+            switch UIDevice.current.batteryState {
+            case .unplugged:
+                showUnpluggedBatteryAlert = true
+            default:
+                checkForExpensiveNetwork()
+            }
+        } else {
+            showNoNetworkAlert = true
+        }
+    }
+    
+    private func checkForExpensiveNetwork() {
+        showUnpluggedBatteryAlert = false
+        if network.isExpensive {
+            showExpensiveNetworkAlert = true
+        } else {loopThroughTripsAndUpload()}
+    }
+    
+    private func loopThroughTripsAndUpload() {
+        // Loop through trips and upload any new/missed files
+        //            upload.resetVars()
+
+    }
+    
+    private func uploadFiles(tripName: String, fileType: String){
+        Task.detached {
+            // Set counters
+            await upload.resetVars()
+//                                    await startInitalTripUpload(trip: item)
+            await upload.getLocalFilePaths(tripName: tripName, folderName: fileType)
+            await upload.uploadAndShowError(tripName: tripName, uploadURL: settings[0].uploadScriptURL)
+//                                    Task.detached {
+//                                        await tryUpload.getLocalFilePaths(tripName: tripName, folderName: "images")
+//                                        await tryUpload.uploadAndShowError(uploadURL: uploadURL)
+//                                    }
+        }
     }
 }
